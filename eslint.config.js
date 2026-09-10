7ba264dd5c09ddde7e7c368789c4bb0c198e24a5
rikunemu <rikuwo794niku@gmail.com>
2026-09-10 23:53:52 +0900

    chore: add ESLint and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier/flat'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist', 'coverage']),
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: globals.node,
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: globals.browser,
+    },
+  },
+  prettier,
+])
