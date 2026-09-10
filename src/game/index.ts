3345ee9234ae8dce5ad4a4d5b0158094a9b14b64
rikunemu <rikuwo794niku@gmail.com>
2026-09-11 00:00:48 +0900

    feat: define initial game state model

---
+++ src/game/index.ts
@@ -0,0 +1,13 @@
+export {
+  GAME_SCREENS,
+  createInitialGameState,
+  initialGameState,
+} from './gameState'
+export type {
+  BattleState,
+  BattleStatus,
+  GameScreen,
+  GameState,
+  PlayerState,
+  ScenarioId,
+} from './gameState'
