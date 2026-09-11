b34fddc63dac3d705d0851e8544f12ec3600e75b
rikunemu <rikuwo794niku@gmail.com>
2026-09-11 12:02:34 +0900

    feat: add investigation history

---
+++ src/investigation/index.ts
@@ -0,0 +1,9 @@
+export { createInvestigationHistory } from './investigationHistory'
+export type {
+  InvestigationClock,
+  InvestigationHistory,
+  InvestigationHistoryEntry,
+  InvestigationHistoryEntryInput,
+  InvestigationResult,
+  InvestigationResultKind,
+} from './investigationHistory'
